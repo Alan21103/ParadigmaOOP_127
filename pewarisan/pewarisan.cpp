b7078d4ff83ccb4a7ce8314844475eb21ebf0cf3
Alan21103 <syahlansaputra03@gmail.com>
2023-05-22 11:34:23 +0700

    membuat perintah menampilkan

--- pewarisan/pewarisan.cpp
+++ pewarisan/pewarisan.cpp
@@ -7,3 +7,15 @@ public:
 	string nama;
 
 	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat/n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus/n" << endl;
+	}
+
+	int jumlah(int a, int b) {
+		return a + b;
+	}
+
+};
